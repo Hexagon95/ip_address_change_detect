36089acc0215db8094f11708025e121b6bc25924
Hexagon95 <vazul95@gmail.com>
2023-05-14 21:51:39 +0200

    Background loop complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'dart:io';
-import 'dart:isolate';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() async{
-  await HandleIPAddress.queryIpAddress;
-  HandleIPAddress.loopBackground;
+  HandleIPAddress.currentIPAddress = await HandleIPAddress.queryIpAddress;
   runApp(
     MaterialApp(      
       initialRoute: 'display',
@@ -14,35 +12,31 @@ void main() async{
       }
     )
   );
+  HandleIPAddress.loopBackgroundIpAddressCheck();
+  if(kDebugMode)print("it seems loop is working?");
 }
 
 class HandleIPAddress { //#######################################################################################################//
   // ----- < Variables [Static]> ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- //
-  static List<NetworkInterface> _interface =  List<NetworkInterface>.empty();
-  static dynamic isolate;
+  static List<NetworkInterface> _interface =      List<NetworkInterface>.empty();
+  static bool enableBackgroundIPAddressCheck =    true;
   static InternetAddress? currentIPAddress;
   static InternetAddress? examingIPAddress;
 
   // ----- < Methods [Static] >  ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- //
-  static Future get queryIpAddress async{
+  static Future<InternetAddress> get queryIpAddress async{
     _interface =        await NetworkInterface.list();
-    currentIPAddress =  _interface[0].addresses.first;
+    return _interface[0].addresses.first;
   }
 
-  static void get loopBackground async{ while(true){
+  static Future loopBackgroundIpAddressCheck() async {while(enableBackgroundIPAddressCheck){
     await Future.delayed(const Duration(seconds: 10));
-    if(kDebugMode)print('Loop is still running');
-  }}//isolate = await Isolate.spawn<void>((message) => _loopBackgroundIpAddressCheck, null);
-
-  // ----- < Methods [2] > ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- //
-  static void _loopBackgroundIpAddressCheck() async {while(true){
-    await Future.delayed(const Duration(seconds: 10));
-    /*_interface =        await NetworkInterface.list();
-    examingIPAddress =  _interface[0].addresses.first;
+    examingIPAddress = await queryIpAddress;
     if(examingIPAddress!.address != currentIPAddress!.address){
-      currentIPAddress = examingIPAddress;                            // If the examined IPAddress is drifferent
-    }*/
-    if(kDebugMode)print('background is running');
+      currentIPAddress =                            examingIPAddress;
+      DisplayState.ipAddressChangeNotifier.value =  currentIPAddress!.address;
+    }
+    DisplayState.counterInt.value++;
   }}
 }
 
@@ -54,12 +48,17 @@ class Display extends StatefulWidget { //#######################################
 }
 
 class DisplayState extends State<Display> { //###################################################################################//
+  // ----- < Variables [Static] > ---- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- //
   // ----- < Variables > - ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- //
-  ValueNotifier<String> ipAddressChangeNotifier = ValueNotifier(HandleIPAddress.currentIPAddress!.address);
-  TextStyle customTextStyle =                     const TextStyle(color: Color.fromARGB(255, 50, 50, 0), fontSize: 20);
+  static ValueNotifier<String> ipAddressChangeNotifier =  ValueNotifier(HandleIPAddress.currentIPAddress!.address);
+  static ValueNotifier<int> counterInt =                  ValueNotifier(0);
+  TextStyle customTextStyle =                             const TextStyle(color: Color.fromARGB(255, 50, 50, 0), fontSize: 20);
 
   // ----- < Constructors > ---- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- //
-  DisplayState() {ipAddressChangeNotifier.addListener(() => setState((){}));}
+  DisplayState() {
+    ipAddressChangeNotifier.addListener(() => setState((){}));
+    counterInt.addListener(() => setState((){}));
+  }
 
   // ----- < Main Widget Build > ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- //
   @override
@@ -76,6 +75,16 @@ class DisplayState extends State<Display> { //##################################
 
   // ----- < Widgets [1] > ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- //
   Widget get _drawBody => Center(child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-    Text('Current IP Address: [ ${ipAddressChangeNotifier.value} ]', style: customTextStyle)
+    Text('Current IP Address: [ ${ipAddressChangeNotifier.value} ]', style: customTextStyle),
+    Text('Loop counter: ${counterInt.value}', style: customTextStyle),
+    _drawButtonTerminateIPAddressCheck
   ]));
+
+  // ----- < Widgets [2] > ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- //
+  Widget get _drawButtonTerminateIPAddressCheck => TextButton(
+    style:      ButtonStyle(backgroundColor: MaterialStateProperty.all((HandleIPAddress.enableBackgroundIPAddressCheck)? Colors.yellow : Colors.grey)),
+    onPressed:  () => setState(() => HandleIPAddress.enableBackgroundIPAddressCheck = false),
+    child:      Text('Terimnate IP Address Check', style: customTextStyle,)
+  );
+
 }
